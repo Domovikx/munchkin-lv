02b9d8ddb814227e0296752f12a44e69224bbbf9
Ilya Ivanouski <domovikx@gmail.com>
2019-03-22 09:51:28 +0300

    chore: trying to make a high textarea

--- assets/js/mine.js
+++ assets/js/mine.js
@@ -16,37 +16,23 @@ $(document).ready(function () {
 
 
 function calculator() {
-    // $('#result').html(textarea);
 
-    let arr = textarea.split(/[^-0-9]/); // подумать - /[^-0-9]*/
-
-
-    for (const a of arr) {
-        console.log('a1', a)
-    }
-
+    let arr = textarea.split(/[^-0-9]/); // подумать - как оптимизировать
 
     let newArr = [];
     for (let i = 0; i < arr.length; i++) {
-        // if (arr[i - 1] == '-' && arr[i] > 0) {
-        //     newArr.push(-arr[i]);
-        // }
         if (arr[i] > 0) {
             newArr.push(+arr[i]);
         } else if (arr[i] <= 0) {
             newArr.push(+arr[i]);
         }
-
     }
 
     let sum = 0;
     for (const a of newArr) {
-        // console.log('a :', a);
         sum += a;
     }
 
     $('#result').html(sum);
-
-
 }
 
