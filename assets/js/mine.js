1373b398bae1176518252768924efe4cef415810
Ilya Ivanouski <domovikx@gmail.com>
2019-03-26 14:39:58 +0300

    feat: add cube algorithm

--- assets/js/mine.js
+++ assets/js/mine.js
@@ -9,25 +9,34 @@ $(document).ready(function () {
     strTextarea += 'You can write any related numbers in this field, separating them with + - / () characters, etc.';
     $("#textarea").html(strTextarea);
     calculator();
-    // хочу сделать вызов автовысоты - autoGrow(element) не знаю как написать
+    autoGrow();
+    diceRandom();
+
 
     // очистка поля textarea
     $('#textarea').one("click", function () {
         let strTextarea = 'Level: 1\n';
-        strTextarea += 'Bonus: \n';
-        strTextarea += 'Curses: ';
+        strTextarea += 'Bon: \n';
+        strTextarea += 'Cur: ';
         $("#textarea").html(strTextarea);
-        autoGrow(this);
         calculator();
+        autoGrow();
     });
+
 
     $('#textarea').bind('input', function () {
         calculator();
+        autoGrow();
     });
 
+
     $("#dice").click(function () {
-        alert("Вы нажали на кнопку кубики " + diceRandom());
+        // alert("Вы нажали на кнопку кубики " + diceRandom());
+        diceRandom();
     });
+
+    
+    $(window).on('beforeunload', function () { return 'Вы уверены, что хотите покинуть страницу?'; });
 
 });
 
@@ -35,11 +44,7 @@ $(document).ready(function () {
 function calculator() {
     textarea = $('#textarea').val();
     let str = textarea.replace(/-/g, ' -');
-    let arr = str.split(/[^-0-9]/); // подумать - как оптимизировать
-    // можно было попробовать сделать 
-    // arr = str.match(/[-0-9]*/g);
-    // console.log('arr', arr)
-
+    let arr = str.split(/[^-0-9]/);
     let newArr = [];
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] > 0) {
@@ -53,17 +58,55 @@ function calculator() {
     for (const a of newArr) {
         sum += a;
     }
-
     $('#result').html(sum);
-
 }
 
-// autoGrow('#textarea'); //хз работает ли это
-function autoGrow(element) {
-    element.style.height = "0px";
-    element.style.height = (element.scrollHeight) + "px";
+
+function autoGrow() {
+    const element = $('#textarea')[0];
+    element.style.height = '0px';
+    element.style.height = (element.scrollHeight) + 'px';
 }
+
 
 function diceRandom() {
-    return Math.floor(Math.random() * 6) + 1;
+
+    const num = Math.floor(Math.random() * 6) + 1;
+
+    const colorBefore = 'antiquewhite';
+    const colorAfter = 'rgba(0, 0, 0, 0.6)';
+
+    switch (num) {
+        case 1: changeColorCircle([5], colorBefore, colorAfter);
+            break;
+        case 2: changeColorCircle([3, 7], colorBefore, colorAfter);
+            break;
+        case 3: changeColorCircle([1, 5, 9], colorBefore, colorAfter);
+            break;
+        case 4: changeColorCircle([1, 3, 7, 9], colorBefore, colorAfter);
+            break;
+        case 5: changeColorCircle([1, 3, 5, 7, 9], colorBefore, colorAfter);
+            break;
+        case 6: changeColorCircle([1, 3, 4, 6, 7, 9], colorBefore, colorAfter);
+            break;
+        default:
+            break;
+    }
+
+    return num;
+}
+
+
+function changeColorCircle(arr, colorBefore, colorAfter) {
+    let color = colorAfter;
+    for (let i = 1; i < 10; i++) {
+        for (const a of arr) {
+            if (a == i) {
+                $("#dice>div:nth-child(" + i + ")").css("background-color", colorAfter);
+                break;
+            } else {
+                $("#dice>div:nth-child(" + i + ")").css("background-color", colorBefore);
+            }
+        }
+    }
 }
